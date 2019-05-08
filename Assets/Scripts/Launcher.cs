3afc240b0633efd897aedfaf7bdc63cf66b13c18
Brandon Pfeifer <thebpfeifer@gmail.com>
2019-05-07 22:16:21 -0500

    Adding basic Photon Launcher code

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviour
+{
+    #region Private Serializable Fields
+
+    #endregion
+
+    #region  Private Fields
+
+    // This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+    string gameVersion = "1";
+
+    #endregion 
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        Connect();
+    }
+
+    #region Public Methods
+
+    public void Connect()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    #endregion
+
+}
