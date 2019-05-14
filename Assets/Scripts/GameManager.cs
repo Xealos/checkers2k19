105e66c59af50661443f48de1ab8958f26fa4542
Brandon Pfeifer <thebpfeifer@gmail.com>
2019-05-13 23:32:07 -0500

    Added GameManager class and prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon Callbacks
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        // not seen if you're the player connecting
+        Debug.LogFormat("OnPlayerEnteredRoom () {0}", other.NickName); 
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+            LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        // seen when other disconnects
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName);
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+            LoadArena();
+        }
+    }
+
+    #endregion
+    // Start is called before the first frame update
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    private void LoadArena()
+    {
+        if (PhotonNetwork.IsMasterClient == false)
+        {
+            Debug.LogError("PhotonNetwork : Trying to load a level be we are not the master client.");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("CheckerboardScene");
+    }
+
+}
