a133679280b43d9c221cbccdbde8c4a222320be4
Brandon Pfeifer <thebpfeifer@gmail.com>
2020-01-19 14:43:01 -0600

    Piece Callback prototyping

---
+++ Assets/Scripts/Game/PieceCallbacks.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PieceCallbacks : MonoBehaviourPunCallbacks
+{
+    public string tempTag; 
+    
+    void Start()
+    {
+        this.GetComponent<PhotonView>().RPC("SyncGameTag", RpcTarget.All, "Hello!");
+    }
+    [PunRPC]
+    void SyncGameTag(string str)
+    {
+        this.tag = str;
+    }
+}
