9b7ef59449e95ae72f18a4501719c59c6fcb4b13
Brandon Pfeifer <thebpfeifer@gmail.com>
2020-01-29 22:34:46 -0600

    Removed prototyped/dead code.

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,7 +4,6 @@ using Photon.Realtime;
 using Photon.Pun.UtilityScripts;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
